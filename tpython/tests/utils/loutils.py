68d0f4b4b14828c8a2d4c3c3009b617c9a63d181
EmmaKidney <ekidney@redhat.com>
2021-11-30 10:39:03 +0000

    Loutils tests imports

--- tpython/tests/utils/loutils.py
+++ tpython/tests/utils/loutils.py
@@ -1,4 +1,12 @@
 from lib.utils.loutils import reapLos, reapLoType, findTopLos, findLos, findTalksWithVideos, publishLos, copyResource 
+from lib.models.lab import Lab
+from lib.models.topic import Topic, Unit
+from lib.models.lo import LearningObject
+from lib.models.course import Course
+from lib.models.los import Archive, PanelTalk, Talk
+from lib.models.weblos import Git, PanelVideo, Video, Web
 import os
 import pytest
+
+
 
